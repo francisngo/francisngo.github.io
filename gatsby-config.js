82b1d2a4442578f61f69f59b495df6cbc73d708d
Francis Ngo <francis.t.ngo@gmail.com>
2020-02-08 12:50:10 -0800

    Set gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,16 @@
+const config = require('./src/config');
+
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: config.siteTitle,
+    description: config.siteDescription,
+    author: config.name,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -13,22 +18,17 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Francis Ngo`,
+        short_name: `FrancisNgo`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/gatsby-icon.png`,
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
 }
---
+++ src/config/index.js
@@ -0,0 +1,26 @@
+module.exports = {
+	siteTitle: 'Francis Ngo | Software Engineer',
+	siteDescription: 'Francis Ngo is a software engineer based in Los Angeles, CA who specializes in developing high-quality websites and applications.',
+	siteKeywords: 'Francis Ngo, Francis, Ngo, francisngo, software engineer, front-end engineer, web developer, javascript, west coast',
+	siteUrl: 'http://francisngo.me',
+	siteLanguage: 'en_US',
+	name: 'Francis Ngo',
+	location: 'Los Angeles, CA',
+	email: 'francis.t.ngo@gmail.com',
+	github: 'https://github.com/francisngo',
+	socialMedia: [
+		{
+			name: 'Github',
+			url: 'https://www.github.com/francisngo',
+		},
+		{
+			name: 'LinkedIn',
+			url: 'https://www.linkedin.com/in/francisngo'
+		},
+		{
+			name: 'Instagram',
+			url: 'https://www.instagram.com/wheresfrancis',
+		},
+	],
+	navHeight: 100,
+}
