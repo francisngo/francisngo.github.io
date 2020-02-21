605f12b49f16d434b5c477a4bd07470c5ef396c2
Francis Ngo <francis.t.ngo@gmail.com>
2020-02-20 17:14:30 -0800

    Add main component styles

---
+++ src/styles/Nav.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+import theme from './theme';
+const { colors } = theme;
+
+const Nav = styled.nav`
+  width: 100%;
+  color: ${colors.black};
+`;
+
+export default Nav;
