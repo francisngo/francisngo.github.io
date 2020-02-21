87c8a195d3a30eb56da9ad2bb1a985c55e920bb6
Francis Ngo <francis.t.ngo@gmail.com>
2020-02-20 17:17:30 -0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,90 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react';
+import { graphql } from 'gatsby';
+import PropTypes from 'prop-types';
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import Layout from '@components/layout';
+import Hero from '@components/hero';
+import About from '@components/about';
+import Contact from '@components/contact';
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+import styled from 'styled-components';
+import { mixins, Main } from '../styles';
+
+const MainContainer = styled(Main)`
+  ${mixins.padding}
+  counter-reset: section;
+`;
+
+const IndexPage = ({ data, location }) => (
+  <Layout location={location}>
+    <MainContainer id="content">
+      <Hero data={data.hero.edges} />
+      <About data={data.about.edges} />
+      <Contact data={data.contact.edges} />
+    </MainContainer>
   </Layout>
-)
+);
 
-export default IndexPage
+IndexPage.propTypes = {
+  data: PropTypes.object.isRequired,
+  location: PropTypes.object,
+};
+
+export default IndexPage;
+
+/* eslint no-undef: off */
+export const query = graphql`
+         query IndexQuery {
+           hero: allMarkdownRemark(
+             filter: { fileAbsolutePath: { regex: "/hero/" } }
+           ) {
+             edges {
+               node {
+                 frontmatter {
+                   title
+                   name
+                   subtitle
+                   contactText
+                 }
+                 html
+               }
+             }
+           }
+           about: allMarkdownRemark(
+             filter: { fileAbsolutePath: { regex: "/about/" } }
+           ) {
+             edges {
+               node {
+                 frontmatter {
+                   title
+                   avatar {
+                     childImageSharp {
+                       fluid(
+                         maxWidth: 700
+                         quality: 90
+                         traceSVG: { color: "#8892b0" }
+                       ) {
+                         ...GatsbyImageSharpFluid_withWebp_tracedSVG
+                       }
+                     }
+                   }
+                   skills
+                 }
+                 html
+               }
+             }
+           }
+           contact: allMarkdownRemark(
+             filter: { fileAbsolutePath: { regex: "/contact/" } }
+           ) {
+             edges {
+               node {
+                 frontmatter {
+                   title
+                 }
+                 html
+               }
+             }
+           }
+         }
+       `;
