2ab6d865a8c64cdb1b707aaffe5a8c019d1abd3e
Francis Ngo <francis.t.ngo@gmail.com>
2020-02-20 17:14:47 -0800

    Add icons

---
+++ src/components/icons/index.js
@@ -0,0 +1,13 @@
+import IconLoader from './loader';
+import IconLogo from './logo';
+import IconGithub from './github';
+import IconLinkedin from './linkedin';
+import IconInstagram from './instagram';
+
+export {
+	IconLoader,
+	IconLogo,
+	IconGithub,
+	IconLinkedin,
+	IconInstagram,
+}
---
+++ src/components/icons/logo.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const IconLogo = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 500 500">
+    <title>Logo</title>
+    <g>
+      <rect x="60" y="60" class="st0" width="380" height="380"/>
+      <path d="M430,70v360H70V70H430 M450,50H50v400h400V50L450,50z"/>
+    </g>
+    <text transform="matrix(1 0 0 1 152.1963 372.1572)" class="st1 st2">F</text>
+  </svg>
+);
+
+export default IconLogo;
