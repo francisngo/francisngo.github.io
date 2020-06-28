5702461af9c60faa365c94188a37f386a098357d
francisngo <francis.t.ngo@gmail.com>
2020-06-27 23:39:53 -0700

    Remove loader

--- src/components/icons/index.js
+++ src/components/icons/index.js
@@ -1,11 +1,9 @@
-import IconLoader from './loader';
 import IconLogo from './logo';
 import IconGithub from './github';
 import IconLinkedin from './linkedin';
 import IconInstagram from './instagram';
 
 export {
-	IconLoader,
 	IconLogo,
 	IconGithub,
 	IconLinkedin,
