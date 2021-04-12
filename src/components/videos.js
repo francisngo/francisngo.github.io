d9e5547fdfd53ff0d8c2d3b0287d09a8ab19a321
Francis Ngo <francis.t.ngo@gmail.com>
2021-04-12 11:58:22 -0700

    Make youtube videos responsive

--- src/components/videos.js
+++ src/components/videos.js
@@ -2,21 +2,27 @@ import React from 'react';
 
 const Videos = () => {
     return (
-        <div className="rn-project-details-area rn-section-gap bg-color-white">
+        <div className="rn-videos-area rn-section-gap">
             <div className="container">
                 <div className="row">
                     <div className="col-lg-12">
                         <div className="row justify-content-center mt--60">
                             <h1>Installing A Prinsu Roof Rack</h1>
-                            <iframe width="1120" height="630" src="https://www.youtube.com/embed/4C5jS6WIN5M" title="Installing a Prinsu Roof Rack" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                            <div className="video">
+                                <iframe width="560" height="315" src="https://www.youtube.com/embed/4C5jS6WIN5M" title="Installing a Prinsu Roof Rack" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                            </div>
                         </div>
                         <div className="row justify-content-center mt--60">
                             <h1>BFGoodrich KO2 - 285/70R17 - Will it Rub?</h1>
-                            <iframe width="1120" height="630" src="https://www.youtube.com/embed/lzKv8RdLQoY" title="BFGoodrich KO2 - 285/70R17 - Will it Rub?" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                            <div className="video">
+                                <iframe width="560" height="315" src="https://www.youtube.com/embed/lzKv8RdLQoY" title="BFGoodrich KO2 - 285/70R17 - Will it Rub?" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                            </div>
                         </div>
                         <div className="row justify-content-center mt--60">
                             <h1>1 Year Ownership and Mods Overview</h1>
-                            <iframe width="1120" height="630" src="https://www.youtube.com/embed/o7Z7JKqT9O8" title="2020 Toyota 4Runner Off Road Premium - 1 Year Ownership and Mods Overview" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                            <div className="video">
+                                <iframe width="560" height="315" src="https://www.youtube.com/embed/o7Z7JKqT9O8" title="2020 Toyota 4Runner Off Road Premium - 1 Year Ownership and Mods Overview" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                            </div>                            
                         </div>
                     </div>
                 </div>
